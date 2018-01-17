cc280be6a6f090dbfd6814280a82cf0211492cd5
simon REVEL <simon.revel@supelec.fr>
2018-01-17 23:34:47 +0100

    bug solved

--- createBook.bat
+++ createBook.bat
@@ -13,13 +13,13 @@ for /f "tokens=1 delims=." %%i in ("!pathPhoto!") do set name=%%i
 move "%%a" "book\page"!nbpage!"\"
 if !count! == 4 echo ^</div^>^<div class="pictures"^> >> book\page!nbpage!Photo.html
 if !count! == 7 echo ^</div^>^<div class="pictures"^> >> book\page!nbpage!Photo.html
-if !count! == 10 echo ^</div^> >> book\page!nbpage!Photo.html
 echo ^<div class="frame"^> ^<img src=page!nbpage!\!name!.jpg alt="photoDip"^> ^</img^>^</div^> >> book\page!nbpage!Photo.html
 if !count! == 4 echo ^</div^>^<div class="names_line"^> >> book\page!nbpage!Name.html
 if !count! == 7 echo ^</div^>^<div class="names_line"^> >> book\page!nbpage!Name.html
-if !count! == 10 echo ^</div^> >> book\page!nbpage!Name.html
 echo ^<div class="names"^> !name! ^</div^> >> book\page!nbpage!Name.html
 set /a count += 1
+if !count! == 10 echo ^</div^>^</div^> >> book\page!nbpage!Name.html
+if !count! == 10 echo ^</div^> >> book\page!nbpage!Photo.html
 if !count! == 10 (set /a count = 1 & echo ^<div class="pageNumber"^>!nbpage!^</div^>^</div^> >> book\page!nbpage!Name.html & set /a nbpage += 1 & if not exist book\page!nbpage! mkdir book\page!nbpage! & copy "pageName.html" "book\page!nbpage!Name.html" & copy "pagePhoto.html" "book\page!nbpage!Photo.html"))
 echo Work done!
 pause > nul
